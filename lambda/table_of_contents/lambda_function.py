6ecb96ae9be18ffacdd47e48a0ab12c4a5d0ebfa
hibiki-kato <katohibiki17@gmail.com>
2023-07-08 17:29:43 +0900

    テスト

--- lambda/table_of_contents/lambda_function.py
+++ lambda/table_of_contents/lambda_function.py
@@ -7,6 +7,7 @@ openai.api_key = os.getenv("OPENAI_API_KEY")
 def lambda_handler(event, context):
     data = event
     theme: str = data["theme"]
+    print("theme読み込み成功")
     #目次の作成(成功するまで10回トライ)
     trial = 10
     for i in range(trial):
