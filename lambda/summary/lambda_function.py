8a43ba49bbde17f6dd4319b06aced93e6c9d0288
hibiki-kato <katohibiki17@gmail.com>
2023-07-09 14:03:16 +0900

    修正

--- lambda/summary/lambda_function.py
+++ lambda/summary/lambda_function.py
@@ -6,16 +6,13 @@ openai.api_key = os.getenv("OPENAI_API_KEY")
 
 def lambda_handler(event, context):
     # data = json.loads(event.get('body', '{}'))
-    data = event
-    theme: str = data['script']
+    script: str = data['script']
     
     #キーポイントとスクリプトの作成
     trial: int = 3 #最大試行回数
-    key_points:str
-    script:str
     for i in range(trial):
         try:
-            summary = generate_summary(summary)
+            summary = generate_summary(sucript)
             break
         except json.JSONDecodeError:
             # chatgptがjsonを返さない場合があるので、その場合は再度トライする
@@ -46,4 +43,6 @@ def generate_summary(script: str) -> str:
         model="gpt-3.5-turbo",
     )
     
-    return response.choices[0].message.content
+    summary = response.choices[0].message.content
+    
+    return summary
