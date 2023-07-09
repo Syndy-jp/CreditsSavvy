52b09819beadfd9169888e2eed7e19d546378699
hibiki-kato <katohibiki17@gmail.com>
2023-07-09 18:03:04 +0900

    エラー

--- lambda/summary/lambda_function.py
+++ lambda/summary/lambda_function.py
@@ -5,8 +5,14 @@ import openai
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
 def lambda_handler(event, context):
-    data: bin = json.loads(event.get('body', '{}'))
-    print(data)
+    try:
+        data: bin = event.get('body', '{}')
+else:
+    return return {        
+            'statusCode': 500,
+            'body': "データを読み込めてない",
+        }
+    
     # data = json.loads(event)
     # mp3: bin = data['mp3']
     
